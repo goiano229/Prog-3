54973ef51be380ebc8e23e320dd420444cc68251
Guilherme Silva <silvaribeiroguilherme2@gmail.com>
2026-09-17 22:10:47 -0300

    Add files via upload

---
+++ atvidade_1/VerdadeiroOuFalso.java
@@ -0,0 +1,33 @@
+/*
+ * Lista de Atividades - Aula 1
+ * Exercício 2: Verdadeiro ou Falso?
+ *
+ * Declara 3 variáveis booleanas (valores escolhidos pelo aluno) e
+ * valida, com if/else, se cada uma é verdadeira ou falsa.
+ */
+public class VerdadeiroOuFalso {
+    public static void main(String[] args) {
+        // Valores definidos pelo aluno:
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+
+        if (a) {
+            System.out.println("A variável 'a' é VERDADEIRA.");
+        } else {
+            System.out.println("A variável 'a' é FALSA.");
+        }
+
+        if (b) {
+            System.out.println("A variável 'b' é VERDADEIRA.");
+        } else {
+            System.out.println("A variável 'b' é FALSA.");
+        }
+
+        if (c) {
+            System.out.println("A variável 'c' é VERDADEIRA.");
+        } else {
+            System.out.println("A variável 'c' é FALSA.");
+        }
+    }
+}
